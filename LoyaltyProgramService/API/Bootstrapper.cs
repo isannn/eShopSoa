f39d65eb14914a887d9dc60578c19f2eacc90408
Aleksei Isaev <isannn@bk.ru>
2022-01-02 17:46:37 +0300

    add loyalty program service
    add empty asp.net core project for loyalty program service

---
+++ LoyaltyProgramService/API/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+
+namespace LoyaltyProgramService.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Bootstrapper: DefaultNancyBootstrapper
+    {
+        protected override Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration
+            => NancyInternalConfiguration.WithOverrides(builder => builder.StatusCodeHandlers.Clear());
+    }
+}
